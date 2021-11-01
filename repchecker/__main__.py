7a5001c8c631ded195e6635194dc68b2f1f95198
n4ch0x <93493610+n4ch0x@users.noreply.github.com>
2021-11-01 03:28:28 +0100

    Add files via upload

---
+++ repchecker/__main__.py
@@ -0,0 +1,13 @@
+"""
+Author:         Nacho Pobes
+Student number: 500855533
+Name:           RepChecker
+Version:        1.0
+"""
+
+import app
+
+
+# Handy to be able to call the application from the console as in `python -m repchecker`
+if __name__ == '__main__':
+    app.main()
